df099502c41c03e01cc13a8ab6435584a1c61e5b
root <sunink1@naver.com>
2021-05-17 09:21:08 +0000

    userinfo modifications completed

--- back/fashion/views/auth.py
+++ back/fashion/views/auth.py
@@ -25,7 +25,7 @@ bp = Blueprint('auth', __name__, url_prefix='/')
 def register():
     if not request.is_json:
         print("check_no_jason")  # 확인용... 나중에 삭제할것
-        return jsonify({"msg": "Missing JSON in request"}), 402
+        return jsonify({"msg": "Missing JSON in request", 'status':400})
 
     else:
         print('check')
@@ -43,15 +43,14 @@ def register():
         # print(email, password, name, nickname)  # 확인용....나중에 삭제할것
 
         emailcheck = models.User.query.filter_by(email=email).first()
-        nicknamecheck = models.User.query.filter_by(
-            nickname=nickname).first()
+        nicknamecheck = models.User.query.filter_by(nickname=nickname).first()
         print(emailcheck)
         if not(name and email and pw and nickname):
-            return jsonify({"msg": "빈칸 오류", 'status': 301})
+            return jsonify({"msg": "빈칸 오류", 'status': 400})
         elif emailcheck is not None:
-            return jsonify({"msg": "이미 가입된 이메일입니다.", 'status': 302})
+            return jsonify({"msg": "이미 가입된 이메일입니다.", 'status': 400})
         elif nicknamecheck is not None:
-            return jsonify({"msg": "닉네임이 존재할때", 'status': 303})
+            return jsonify({"msg": "닉네임이 존재할때", 'status': 400})
         else:
             if checkvalid.passwordCheck(pw) == 1:
                 hashpw = bcrypt.hashpw(
@@ -68,19 +67,19 @@ def register():
                 )
                 models.db.session.add(user)
                 models.db.session.commit()
-                return jsonify({"msg": "회원가입 성공", 'status': 300})
+                return jsonify({"msg": "회원가입 성공", 'status': 200})
             elif checkvalid.passwordCheck(pw) == 2:
 
                 return jsonify({'msg': '비밀번호 기준에 맞지 않습니다. 비밀번호는 8자이상, 숫자+영어+특수문자 조합으로 이루어집니다.', 'status': 304})
             else:
-                return jsonify({'msg': '비밀번호는 하나이상의 특수문자가 들어가야합니다', 'status': 305})
+                return jsonify({'msg': '비밀번호는 하나이상의 특수문자가 들어가야합니다', 'status': 400})
 
 
 @bp.route('/sign-in', methods=['POST'])
 @swag_from("../swagger_config/register.yml")
 def login():
     if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 402
+        return jsonify({"msg": "Missing JSON in request", 'status':400})
     else:
         print('check')
         # body = literal_eval(request.get_json()['body'])
@@ -90,16 +89,16 @@ def login():
         pw = body['pw']
 
         if not email:
-            return jsonify({"msg": "아이디 치세요", 'status': 401})
+            return jsonify({"msg": "아이디 치세요", 'status': 400})
 
         elif not pw:
-            return jsonify({"msg": "비번 치세요", 'status': 401})
+            return jsonify({"msg": "비번 치세요", 'status': 400})
 
         queried = models.User.query.filter_by(email=email).first()
         print('checkpw:', queried.pw)
         if bcrypt.checkpw(pw.encode('utf-8'), queried.pw.encode('utf-8')):
             # Identity can be any data that is json serializable
-            access_token = create_access_token(identity=queried.id)
+            access_token = create_access_token(identity=queried.id, fresh=True)
             refresh_token = create_refresh_token(identity=queried.id)
             print('ok')
             print(queried.id, queried.nickname)
@@ -117,18 +116,64 @@ def login():
                 'access_token': access_token,
                 'refresh_token': refresh_token,
                 'user_object': user_object,
-                'status': 400
+                'status': 200
             })
         else:
-            return jsonify({"msg": "비밀번호 불일치", "status": 401})
+            return jsonify({"msg": "비밀번호 불일치", "status": 400})
 
+@bp.route('/modification', methods=['POST'])
+@swag_from("../swagger_config/register.yml")
+# @jwt_required()
+def modify():
+    if not request.is_json:
+        print("check_no_jason")  # 확인용... 나중에 삭제할것
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    else:
+        print('check')
+        # body = literal_eval(request.get_json()['body'])
+        body=request.get_json()
+        
+        userid = body['id']
+        email = body['email']
+        pw = body['pw']
+        name = body['name']
+        nickname = body['nickname']
+        
+        hashpw = bcrypt.hashpw(
+                    pw.encode('utf-8'), bcrypt.gensalt())
+
+        print(userid, email, hashpw, name, nickname)
+        admin=models.User.query.filter_by(id=userid).first()
+
+        emailcheck=models.User.query.filter_by(email=email).first()
+        nicknamecheck=models.User.query.filter_by(nickname=nickname).first()
+
+        print(admin.email)
+        if admin.email != email and emailcheck is None :
+            admin.email=email
+            models.db.session.commit()
+
+        if admin.pw != pw:
+            admin.pw=hashpw
+            models.db.session.commit()
+
+        if admin.name != name:
+            admin.name=name
+            models.db.session.commit()
+
+        if admin.nickname != nickname and nicknamecheck is None:
+            admin.nickname=nickname
+            models.db.session.commit()
+
+        return jsonify({"msg": "회원변경 완료", "status": 200})
 
 @bp.route("/refresh", methods=["POST"])
 # @swag_from("swagger_config/random_letters.yml")
 @jwt_required(refresh=True)
 def refresh():
     identity = get_jwt_identity()
-    access_token = create_access_token(identity=queried.id, fresh=False)
+    access_token = create_access_token(identity=identity, fresh=False)
     return jsonify(access_token=access_token)
 
 
@@ -138,3 +183,4 @@ def refresh():
 @jwt_required(fresh=True)
 def protected():
     return jsonify(foo="bar")
+
