313c43703ac1ebc85c02f1b136804304393dd034
root <sunink1@naver.com>
2021-05-19 06:17:59 +0000

    keyword search completed

--- back/fashion/__init__.py
+++ back/fashion/__init__.py
@@ -32,9 +32,9 @@ def create_app():
     CORS(app, supports_credentials=True)
     # 블루프린트
 # --------------------------------------------------------------------------- #
-    from .views import auth
+    from .views import auth, search
     app.register_blueprint(auth.bp)
-    # app.register_blueprint(search.bp)
+    app.register_blueprint(search.bp)
 
     
     app.config['JWT_SECRET_KEY'] = 'fashion'
