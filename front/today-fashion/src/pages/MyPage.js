c4de5b0991b64b41d4bf5628f52691b3dd67ee14
Nam Dayoung <ramda4415@gmail.com>
2021-05-20 08:47:01 +0000

    Refactor: UserInfo 컴포넌트 분리 시도
    - 이하동문

--- front/today-fashion/src/pages/MyPage.js
+++ front/today-fashion/src/pages/MyPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import UserInfoField from '../components/UserInfoField';
 import UserInfoForm from '../components/UserInfoForm';
 
 const MyPage = () => {
   //비밀번호 입력
   //회원정보 보기 , 회원정보 수정
+  //회원탈퇴 -> 페이지 새로 만들어야 할듯
   const [isConfirmed, setIsConfirmed] = useState(true);
   //테스트 중. 추후 false로 바꿀 것
   const [editInfo, setEditInfo] = useState(false);
@@ -31,9 +33,12 @@ const MyPage = () => {
 
   const handleUpdateUserInfo = async (data) => {
     console.log(data);
+    setEditInfo(false);
   };
   return (
     <div className="container">
+      <Link to="/signout">회원탈퇴</Link>
+      {/* 아래 내용 새로운 컴포넌트로 관리 */}
       {editInfo ? (
         <UserInfoForm
           handleUserInfoForm={handleUpdateUserInfo}
