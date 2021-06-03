ce9a190018345fa94a6e6e62b6596aa9ccbd2caa
Nam Dayoung <ramda4415@gmail.com>
2021-06-03 07:09:40 +0000

    Docs: 의문점 주석 추가
    - 무한스크롤 컴포넌트 속도반영

--- front/today-fashion/src/components/InfiniteProducts.js
+++ front/today-fashion/src/components/InfiniteProducts.js
@@ -69,21 +69,26 @@ const InfiniteProducts = () => {
       return delta;
     };
   })();
-
+  // const checkScrollSpeed = initializeScroll();
   //즉시 실행을 해서 익명함수의 리턴 부분에 정의된 함수를 checkScrollSpeed에 할당
 
-  const handleScrollSpeed = useCallback(() => {
-    // const checkScrollSpeed = initializeScroll();
+  const handleScrollSpeed = () => {
+    // requestData.dataSize의 변화가 있어도 handleScrollSpeed는 못 알아챔
+    // useCallback도 사용해봤으나 소용없음
+
+    // speed를 state로 설정하면? 너무 자주 re-render 될 것 같아서 안됨,
+    // 동기적으로 작동하므로 즉시 speed 변화를 알아채지 못하지 않나?
     const speed = checkScrollSpeed();
     console.log(speed, requestData.dataSize);
     if (speed >= 200 && requestData.dataSize === 10) {
       console.log('dataSize 증가');
       setRequestData({ ...requestData, dataSize: 20 });
-    } else if (speed < 200 && requestData.dataSize === 20) {
+    }
+    if (speed < 200 && requestData.dataSize === 20) {
       console.log('dataSize 감소');
       setRequestData({ ...requestData, dataSize: 10 });
     }
-  }, [requestData, checkScrollSpeed]);
+  };
 
   useEffect(() => {
     //새로고침 없이 re-render만 되면 scroll 이벤트가 계속 붙음...
