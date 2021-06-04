4b4d2da2c6f4f80607d74786ec7989c6609c2325
dainlinda <dainlinda@gmail.com>
2021-06-04 08:32:59 +0000

    Refactor: update cardgame.py
    - mockup data를 db data로 전부 변경

--- back/fashion/views/cardgame.py
+++ back/fashion/views/cardgame.py
@@ -27,93 +27,32 @@ def backcard():
     if not request.is_json:
         return error_code.missing_json_error
     else:
-        body=request.get_json()
+        body = request.get_json()
         limit_num = body['limitNum']
 
         bg_products = models.Product.query.order_by(func.rand()).limit(limit_num).all()
-        bg_products_list = []
+        products_list = []
 
-        asins = ['0764443682',
-                '1291691480',
-                '1940280001',
-                '1940735033',
-                '1940967805',
-                '1942705034',
-                '3293015344',
-                '5378828716',
-                '6041002984',
-                '630456984X',
-                '7106116521',
-                '8037200124',
-                '8037200221',
-                '8279996567',
-                '9239282785',
-                '9239281533',
-                '9269808971',
-                '9654263246',
-                'B00004T3SN',
-                'B00005OTJ8']
-        titles = ['Slime Time Fall Fest [With CDROM and Collector Cards and Neutron Balls, Incredi-Ball and Glow Stick Necklace, Paper Fram',
-                    "XCC Qi promise new spider snake preparing men's accessories alloy fittings magnet buckle bracelet jewelry",
-                    'Magical Things I Really Do Do Too!',
-                    'Ashes to Ashes, Oranges to Oranges',
-                    'Aether & Empire #1 - 2016 First Printing Comic Book Special Edition - Rare! - Blue Juice Comics',
-                    '365 Affirmations for a Year of Love, Peace & Prosperity',
-                    'Blessed by Pope Benedetto XVI Wood Religious Bracelet with Black and White pictures Wood',
-                    'Womens Sexy Sleeveless Camouflage Print Casual High Waist Bodycon Jumpsuit Sportswear',
-                    "Sevendayz Men's Shady Records Eminem Hoodie Hoody Black Medium",
-                    "Dante's Peak - Laserdisc",
-                    'Milliongadgets(TM) Earring Safety Backs For Fish Hook Small Earrings (150)',
-                    'Envirosax Kids Series Jessie & Lulu',
-                    'Envirosax Greengrocer Series Bag 7 Guava',
-                    'Blessed by Pope Benedetto XVI Our Lady of Guadalupe Rose Scented Rosary Rosario Olor a Rosas',
-                    'Tideclothes ALAGIRLS Strapless Beading Homecoming Prom Dresses Short Tulle Formal Gowns White 16',
-                    'ALAGIRLS Strapless Beading Homecoming Prom Dresses Short Tulle Formal Gowns Grape 26Plus',
-                    'Syma S107C 3channel Coaxial Mini Spy Cam Helicopter (White) **MICRO SD CARD NOT INCLUDED**',
-                    'X. L. Carbon Fiber Money Clip, made in the USA',
-                    'Shimmer Anne Shine Clip On Costume/Halloween Cat Ears',
-                    'SpongeBob Squarepants Comforter - Twin']
-        # for bg_product in bg_products:
-        #     print(bg_product)
-        #     bg_products_list.append({'productTitle': bg_product.title, 'productImage': address_format.img(asin[i])})
-        # return {
-        #         'requestNum': limit_num,
-        #         'totalNum': len(bg_products),
-        #         'productsList': bg_products_list
-        #         }, 200
-
-        for i in range(20):
-            bg_products_list.append({'productTitle': titles[i], 'productImage': address_format.img(asins[i])})
+        products_list = [
+            {
+                'productTitle': bg_product.title,
+                'productImage': address_format.img(bg_product.asin)
+            } for bg_product in bg_products
+        ]
 
         return {
-                'requestNum': 20,
-                'totalNum': 20,
-                'productsList': bg_products_list
+                'requestNum': limit_num,
+                'totalNum': len(products_list),
+                'productsList': products_list
                 }, 200
-        # return {
-        #         "productsList": [
-        #             {
-        #             "productImage": address_format.img(asin[0]),
-        #             "productTitle": "womens blue popular shirts"
-        #             },
-        #             {
-        #             "productImage": address_format.img(asin[1]),
-        #             "productTitle": "Womens blue popular shirts"
-        #             }
-        #         ],
-        #         "requestNum": 5,
-        #         "totalNum": 2
-        #         }, 200
+
 
 # api 문서화-----------------------------------------------제작은 아직 안 들어감!
-
-# 1,2,3,5,10,20,30,40,50(문구 10개 중 돌리거나)
 # 3번 뒤 상품 준비가 됐다고 팝업이 뜸
 # 백엔드에서 유저에게 맞는 상품리스트 만들면 프론트에 push를 줄건지 프론트에서 rule base
 # 게임 플레이 누적 횟수
 # 메인카드 10개 단위로
 # 결과보기 했을 때 발전시키게
-
 
 # 메인 카드 api
 @bp.route('/maincard', methods=['GET','POST'])
@@ -125,119 +64,43 @@ def maincard():
         # 별점이 3점 이상인 제품중 유저 선호키워드와 맞는것
         # 신성님 알고리즘=> 누적된 키워드 보내주면 제품 asin 보내줌(?)
         # => 알고리즘을 자세히 알아야...
-        #
-        # if asin in models.ProductUserPlayed.query.filter_by(user_id=user_id).all():
-            # 이미 user가 본 카드는 return 안 함!
-            # 새로이 제품 asin 받아오기
-            # while로 돌려줘야할 듯
-            # 알고리즘이 어떤 식으로 결과가 나와야 완성 가능
-        # else: # 본 카드가 아니라면 결과 반환
+        user_id = get_jwt_identity()
 
-        # 게임 플레이 횟수 ProductUserPlayed 테이블에서 len(user가 플레이한 product 갯수) 하면 될듯
-        # len(models.ProductUserPlayed.query.filter_by(user_id=user_id).all())
-        # 1,2,3,5,10,20,30,40,50(문구 10개 중 돌리거나)
-        # 지금은 랜덤으로 뜨게 해놓음
-        # len(models.ProductUserPlayed.query.filter_by(user_id=user_id).all()) == 0이면 첫 게임
+        ai_result = ['0764443682','1291691480','1940280001','1940735033','1940967805',
+                    '1942705034','3293015344','5378828716','6041002984','630456984X','7106116521',
+                    '8037200124','8037200221','8279996567','9239282785','9239281533','9269808971',
+                    '9654263246','B00004T3SN','B00005OTJ8']
 
-        user_play_num = random.randint(0,8)
-        bg_sentence_list = ['당신의 스타일이면 좋아요를 눌러주세요!', # 1
-        '이런 스타일은 어떠세요?', # 2
-        '스타일 평가를 많이 할 수록 추천이 정확해져요!', # 3
-        '게임 5번 플레이 하면 뜨는 문구에요!',  # 5
-        '게임 10번 플레이 하면 뜨는 문구에요!', # 10
-        '게임 20번 플레이 하면 뜨는 문구에요!', # 20
-        '게임 30번 플레이 하면 뜨는 문구에요!', # 30
-        '게임 40번 플레이 하면 뜨는 문구에요!', # 40
-        '게임 50번 플레이 하면 뜨는 문구에요! 다영님 최고에요' # 50
-        ]
+        # 유저가 플레이한 제품 제외하기
+        products_user_played = models.ProductUserPlayed.query.filter_by(user_id=user_id).all()
 
+        asins_user_played = [product_user_played.asin for product_user_played in products_user_played]
+        print(asins_user_played)
+
+        asins_user_not_played = [x for x in ai_result if x not in asins_user_played]
+        print(asins_user_not_played)
+
+        user_play_num = len(asins_user_played) # user 게임 플레이 횟수
+
+        # user가 플레이 하지 않은 게임 중 결과 반환
         # 제품 10개 안될때 예외 처리 해주기
-        keywords = [['flower', 'dress', 'red', 'summer', 'womens'],
-                    ['blue', 'womens', 'shirts', 'popular'],
-                    ['green', 'poledance', 'top', 'sports'],
-                    ['flower', 'pink', 'winter', 'mens'],
-                    ['idk', 'what', 'to', 'type', 'anymore'],
-                    ['five', 'more', 'left', 'omg'],
-                    ['you', 'might', 'not', 'understand', 'whatIM', 'typing'],
-                    ['ok', 'now', 'three', 'products', 'left'],
-                    ['Taro', 'milk', 'tea', 'is', 'JMT'],
-                    ['finally', 'this', 'is', 'last', 'one'],
-                    ['flower', 'dress', 'red', 'summer', 'womens'],
-                    ['blue', 'womens', 'shirts', 'popular'],
-                    ['green', 'poledance', 'top', 'sports'],
-                    ['flower', 'pink', 'winter', 'mens'],
-                    ['idk', 'what', 'to', 'type', 'anymore'],
-                    ['five', 'more', 'left', 'omg'],
-                    ['you', 'might', 'not', 'understand', 'whatIM', 'typing'],
-                    ['ok', 'now', 'three', 'products', 'left'],
-                    ['Taro', 'milk', 'tea', 'is', 'JMT'],
-                    ['finally', 'this', 'is', 'last', 'one']]
-
-        asins = ['0764443682',
-                '1291691480',
-                '1940280001',
-                '1940735033',
-                '1940967805',
-                '1942705034',
-                '3293015344',
-                '5378828716',
-                '6041002984',
-                '630456984X',
-                '7106116521',
-                '8037200124',
-                '8037200221',
-                '8279996567',
-                '9239282785',
-                '9239281533',
-                '9269808971',
-                '9654263246',
-                'B00004T3SN',
-                'B00005OTJ8']
-        titles = ['Slime Time Fall Fest [With CDROM and Collector Cards and Neutron Balls, Incredi-Ball and Glow Stick Necklace, Paper Fram',
-                    "XCC Qi promise new spider snake preparing men's accessories alloy fittings magnet buckle bracelet jewelry",
-                    'Magical Things I Really Do Do Too!',
-                    'Ashes to Ashes, Oranges to Oranges',
-                    'Aether & Empire #1 - 2016 First Printing Comic Book Special Edition - Rare! - Blue Juice Comics',
-                    '365 Affirmations for a Year of Love, Peace & Prosperity',
-                    'Blessed by Pope Benedetto XVI Wood Religious Bracelet with Black and White pictures Wood',
-                    'Womens Sexy Sleeveless Camouflage Print Casual High Waist Bodycon Jumpsuit Sportswear',
-                    "Sevendayz Men's Shady Records Eminem Hoodie Hoody Black Medium",
-                    "Dante's Peak - Laserdisc",
-                    'Milliongadgets(TM) Earring Safety Backs For Fish Hook Small Earrings (150)',
-                    'Envirosax Kids Series Jessie & Lulu',
-                    'Envirosax Greengrocer Series Bag 7 Guava',
-                    'Blessed by Pope Benedetto XVI Our Lady of Guadalupe Rose Scented Rosary Rosario Olor a Rosas',
-                    'Tideclothes ALAGIRLS Strapless Beading Homecoming Prom Dresses Short Tulle Formal Gowns White 16',
-                    'ALAGIRLS Strapless Beading Homecoming Prom Dresses Short Tulle Formal Gowns Grape 26Plus',
-                    'Syma S107C 3channel Coaxial Mini Spy Cam Helicopter (White) **MICRO SD CARD NOT INCLUDED**',
-                    'X. L. Carbon Fiber Money Clip, made in the USA',
-                    'Shimmer Anne Shine Clip On Costume/Halloween Cat Ears',
-                    'SpongeBob Squarepants Comforter - Twin']
-
-        # products_list = []
-        # for i in range(20):
-            # products_list.append({'keywords': keywords[i],'image': address_format.img(asins[i]), 'title': titles[i], 'asin': asins[i]})
-
-        # Pythonic한 문법
-        products_list = [
-            {
-                'keywords': keywords[i],
-                'image': address_format.img(asins[i]),
-                'title': titles[i],
-                'asin': asins[i]
-            } for i in range(20)
-        ]
-
-        if user_play_num == 0:
-            first_play = True
-        else:
-            first_play = False
+        products_list = []
+        for asin in asins_user_not_played:
+            keywords = [product_keyword.product_keyword for product_keyword in models.ProductKeyword.query.filter_by(asin=asin).all()]
+            product_title = models.Product.query.filter_by(asin=asin).first().title
+            products_list.append({
+                'keywords': keywords if len(keywords) <= 6 else keywords[:6],
+                'image': address_format.img(asin),
+                'title': product_title,
+                'asin': asin
+            })
 
         return {
-            'firstPlay': first_play,
-            'bgSentence': bg_sentence_list[user_play_num],
+            'firstPlay': True if user_play_num == 0 else False,
+            'userPlayNum': user_play_num,
             'products': products_list
             }, 200
+
 
         # 만약 모든 제품을 user가 다봤을 경우 return "msg": no product available 이런거 보내주기
         # Product_keyword_match 테이블, 유저 테이블, 유저와 유저가 이미 선택한 상품을 매칭시켜주는 Product_user_match 테이블(컬럼: user id, product_id)
@@ -255,7 +118,6 @@ def maincard():
             product_asin = body['asin']
             love_or_hate = body['loveOrHate']
 
-            # 아직 db 없어서 주석 처리
             product_user_played = models.ProductUserPlayed(
                 user_id = user_id,
                 asin = product_asin,
@@ -278,61 +140,37 @@ def maincard():
 @jwt_required()
 @swag_from('../swagger_config/result_cards.yml')
 def result_cards():
-    # bookmarks = models.Product_user_match.query.all()
-    # products = models.Product.query.all()
 
-    asin = ['B00HGRB3CE', 'B07PGCYWRJ', '1975421663']
+    ai_result = ['0764443682','1291691480','1940280001','1940735033','1940967805',
+                    '1942705034','3293015344','5378828716','6041002984','630456984X','7106116521',
+                    '8037200124','8037200221','8279996567','9239282785','9239281533','9269808971',
+                    '9654263246','B00004T3SN','B00005OTJ8']
+
+    user_id = get_jwt_identity()
+    products_list = []
+
+    for asin in ai_result:
+        bookmark = models.Bookmark.query.filter_by(asin=asin, user_id=user_id).first()
+        keywords = [product_keyword.product_keyword for product_keyword in models.ProductKeyword.query.filter_by(asin=asin).all()]
+        product = models.Product.query.filter_by(asin=asin).first()
+        product_review = models.ProductReview.query.filter_by(asin=asin).first()
+        pos_review_rate = product_review.positive_review_number / (product_review.positive_review_number + product_review.negative_review_number)
+        products_list.append({
+            'keywords': keywords if len(keywords) <= 6 else keywords[:6],
+            'asin': asin,
+            'price': product.price,
+            'bookmark' : True if bookmark else False, # 존재하면 True 아니면 False
+            'nlpResults': {
+                            'posReviewSummary': product_review.positive_review_summary,
+                            'negReviewSummary': product_review.negative_review_summary
+                        },
+            'starRating': round(product.rating, 2),
+            'posReveiwRate': round(pos_review_rate, 2),
+            'image': address_format.img(asin),
+            'productUrl': address_format.product(asin),
+            'title': product.title
+        })
     return {
-            'productsNum': 3,
-            'products':
-                [
-                    {
-                        'keywords': ['flower', 'dress', 'red', 'summer', 'womens'],
-                        'asin': asin[0],
-                        'price': 300000.00,
-                        'bookmark': True,
-                        'nlpResults': {
-                                            'posReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .',
-                                            'negReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .'
-                                        },
-                        'starRating': 3.24,
-                        'posReveiwRate': 0.50,
-                        'negReviewRate': 0.50,
-                        'image': address_format.img(asin[0]),
-                        'productUrl': address_format.product(asin[0]),
-                        'title': 'women\'s flower sundress'
-                    },
-                    {
-                        'keywords': ['flower', 'pants', 'green', 'winter', 'womens'],
-                        'asin': asin[1],
-                        'price': 1000.20,
-                        'bookmark': False,
-                        'nlpResults': {
-                                            'posReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .',
-                                            'negReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .'
-                                        },
-                        'starRating': 4.05,
-                        'posReveiwRate': 0.50,
-                        'negReviewRate': 0.50,
-                        'image': address_format.img(asin[1]),
-                        'productUrl': address_format.product(asin[1]),
-                        'title': 'women\'s flower green pants'
-                    },
-                    {
-                        'keywords': ['flower', 'dress', 'red', 'summer', 'womens'],
-                        'asin': asin[2],
-                        'price': 300000.00,
-                        'bookmark': True,
-                        'nlpResults': {
-                                        'posReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .',
-                                        'negReviewSummary': 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building . It was the first structure to reach a height of 300 metres . It is now taller than the Chrysler Building in New York City by 5.2 metres (17 ft) Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France .'
-                                     },
-                        'starRating': 5.00,
-                        'posReveiwRate': 0.50,
-                        'negReviewRate': 0.50,
-                        'image': address_format.img(asin[2]),
-                        'productUrl': address_format.product(asin[2]),
-                        'title': 'women\'s flower sundress'
-                    },
-                ]
+            'productsNum': len(products_list),
+            'products': products_list
             }, 200
