8b77e0c9ab282b92016ed7d90bca75d3788bc576
Nam Dayoung <ramda4415@gmail.com>
2021-06-05 09:05:31 +0000

    Fix:game.js 업데이트
    - recoil로 정보 관리
    - 게임 5번 진행 후 결과 페이지 버튼 렌더링

--- front/today-fashion/src/pages/Game.js
+++ front/today-fashion/src/pages/Game.js
@@ -1,21 +1,30 @@
 import axios from 'axios';
 import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import GameCard from '../components/GameCard';
 import { SERVER_URL } from '../config';
+import {
+  gameBackGroundImgs,
+  gameCount,
+  gameQuestionsData,
+} from '../states/state';
 
 const Game = () => {
   //TODO: 첫 게임일 경우 게임 3번 이상부터 결과보기 버튼 생성
   const AuthStr = `Bearer ${localStorage.getItem('access_token')}`;
-  const limitNum = 5;
-  const [background, setBackground] = useState(); //배경 이미지
-  const [questions, setQuestions] = useState(); //배경문구 , 게임문항
+  const limitNum = 10;
+  const [background, setBackground] = useRecoilState(gameBackGroundImgs); //배경 이미지
+  const [questions, setQuestions] = useRecoilState(gameQuestionsData); //배경문구 , 게임문항
+  const [totalPlayNum, setTotalPlayNum] = useRecoilState(gameCount);
+  const [initPlayNum, setInitPlayNum] = useState(0);
   const [questionIdx, setQuestionIdx] = useState(0);
   // 현재 요청한 게임중에서 몇번째 게임 진행중인지 (0-9)
   const [isPending, setIsPending] = useState(true);
   // isPending === true 이면 로딩중
   const [fetchQuestionCount, setFetchQuestionCount] = useState(0);
   // maincard api에 get 요청 한 횟수
+  const [isMore, setIsMore] = useState(true);
 
   axios.defaults.baseURL = SERVER_URL;
   axios.defaults.headers.common['Authorization'] = AuthStr;
@@ -32,7 +41,12 @@ const Game = () => {
   const getQuestions = useCallback(async () => {
     try {
       const res = await axios.get('/maincard');
-      setQuestions(res.data);
+      if (res.data.products.length === 0) {
+        setIsMore(false);
+        setIsPending(false);
+      }
+      setQuestions(res.data.products);
+      setInitPlayNum(res.data.userPlayNum);
       console.log(res);
     } catch (error) {
       console.log(error);
@@ -58,10 +72,9 @@ const Game = () => {
         setQuestions();
         setQuestionIdx(0);
         getQuestions();
-        setFetchQuestionCount(fetchQuestionCount + 1);
       }
     },
-    [questionIdx, sendAnswer, getQuestions, fetchQuestionCount]
+    [questionIdx, sendAnswer, getQuestions]
   );
   useEffect(() => {
     getBackGroundData();
@@ -69,22 +82,26 @@ const Game = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const totalPlayNum = fetchQuestionCount * 10 + questionIdx + 1;
+  useEffect(() => {
+    if (initPlayNum && questionIdx) {
+      setTotalPlayNum(initPlayNum + questionIdx + 1);
+    }
+  }, [initPlayNum, questionIdx]);
+
+  console.log(totalPlayNum);
 
   return (
     <div className="game-container">
       <div>게임화면</div>
       <div>{isPending && '로딩중'}</div>
-      <div>
-        {totalPlayNum ? totalPlayNum : null}
-        {questions && questions.bgSentence}
-      </div>
-      {!isPending && totalPlayNum && (
+      <div>{isPending ? null : totalPlayNum}</div>
+      {totalPlayNum > 5 && (
         <Link to="/main">
           <input type="button" value="see result" />
         </Link>
       )}
-      {/* 결과보기 버튼 제한 더 붙이기 */}
+      {!isMore && 'No game left!'}
+      {/* pending일때도 result 보기 할 수 있게 수정*/}
       <div className="background">
         {background &&
           background.map((product, idx) => (
@@ -95,8 +112,9 @@ const Game = () => {
             />
           ))}
       </div>
-      {questions?.products &&
-        questions.products.map((question, idx) => {
+      {questions &&
+        isMore &&
+        questions.map((question, idx) => {
           let zIndex = 10 - idx;
           if (idx < questionIdx) {
             zIndex *= -1;
