498932f634fb39d1e9b9593d938392f4e356d56c
황정우 <ltxctdbnn@gmail.com>
2021-06-05 08:54:05 +0000

    Update front/today-fashion/src/pages/UserInfo.js, front/today-fashion/src/pages/Withdraw.js files

--- front/today-fashion/src/pages/Withdraw.js
+++ front/today-fashion/src/pages/Withdraw.js
@@ -2,19 +2,12 @@ import axios from 'axios';
 import { useCallback } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
-<<<<<<< HEAD
-=======
 import MypageNav from '../components/MypageNav';
->>>>>>> feature_UI/UX
 import { SERVER_URL } from '../config';
 import { userNick } from '../states/state';
 
 const WithDraw = () => {
-<<<<<<< HEAD
-  const [user, setUser] = useRecoilState(userNick);
-=======
   // const [user, setUser] = useRecoilState(userNick);
->>>>>>> feature_UI/UX
   const history = useHistory();
   const handleWithdrawal = useCallback(async () => {
     const AuthStr = `Bearer ${localStorage.getItem('access_token')}`;
@@ -33,18 +26,10 @@ const WithDraw = () => {
   }, [history]);
   return (
     <>
-<<<<<<< HEAD
-      <button disabled>withdraw</button>
-      <Link to="/mypage/userinfo">
-        <button>user info</button>
-      </Link>
-      <div className="withdraw-container">
-        {/* <h3>{user}, I thought we were friends...</h3> */}
-        {/* 위 내용은 protect api에서 userNick을 줘야 사용할 수 있음. */}
-=======
+
       <MypageNav />
       <div className="withdraw-container">
->>>>>>> feature_UI/UX
+
         <p>Are you sure you want to withdraw?</p>
         <button onClick={handleWithdrawal}>yes</button>
         <Link to="/main">
