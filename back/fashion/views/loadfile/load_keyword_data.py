c5d6c280853e6593abc3994cafb83d4a91bb9290
root <sunink1@naver.com>
2021-06-07 16:09:21 +0000

    main

--- back/fashion/views/loadfile/load_keyword_data.py
+++ back/fashion/views/loadfile/load_keyword_data.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 import csv
 from usedb import UseDB
+import pymysql
 db = UseDB()
+
 
 with open('productkeyword.csv', 'r', encoding='UTF8') as f:
     reader = csv.DictReader(f)
 
     for row in reader:
-        db.product_keyword_insert(row['asin'],row['type'],row['keyword'])
+        asin_id=db.product_select(row['asin'])
+
+
+        db.product_keyword_insert(asin_id, row['asin'],row['type'],row['keyword'])
 
 
 db.db_free()
