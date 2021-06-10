a659aec734c66fe7700df053d91405355ea012dc
Nam Dayoung <ramda4415@gmail.com>
2021-06-10 10:57:25 +0000

    Fix: 404 - 404 모든 케이스 동작하게 수정

--- front/today-fashion/src/App.js
+++ front/today-fashion/src/App.js
@@ -88,9 +88,7 @@ function App() {
           render={(props) => <SharedWardrobe {...props} />}
         />
         <Route path="/components" component={ComponentsChart} />
-        <Route path="/404" component={GenericNotFound} />
-        <Redirect to="/404" />
-        {/* Generic Not Found에 404 페이지 넣고 위 주석 해제 */}
+        <Route component={GenericNotFound} />
       </Switch>
     </div>
   );
